a1a78d810a13dfb484fa0160a9ac18135d862879
Niki Mirai <s1411400@coins.tsukuba.ac.jp>
2017-08-01 18:04:55 +0900

    dbpediaの意味をもつurlのみを重複を許さず取得

--- UsePySpotAndRequests.py
+++ UsePySpotAndRequests.py
@@ -1,5 +1,22 @@
+# -*- coding:utf-8 -*-
+
 import requests
+import re
+
 
 payload = {"text":"President Obama called Wednesday on Congress to extend a tax break for students included in last year's economic stimulus package, arguing that the policy provides more generous assistance.","confidence":0.35}
 r = requests.get('http://model.dbpedia-spotlight.org/en/annotate',params=payload)
-print(r.text)
+
+webPage = r.text
+
+pattern = re.compile("http://dbpedia.org/resource/[^\"]+|http://dbpedia.org/ontology/[^\"]+")
+matchList = re.findall(pattern, webPage)
+
+
+matchListUniq = []
+for x in matchList:
+	if x not in matchListUniq:
+		matchListUniq.append(x)
+
+print(matchListUniq)
+
